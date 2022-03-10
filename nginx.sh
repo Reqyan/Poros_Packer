2ec4330b6711c8587d72e9410e0913f2c548ef6b
Reqyan <45092717+Reqyan@users.noreply.github.com>
2022-03-11 02:00:31 +0700

    .

--- nginx.sh
+++ nginx.sh
@@ -3,6 +3,6 @@ while [ ! -f /var/lib/cloud/instance/boot-finished ]
 do echo 'Waiting for cloud-init...'
 sleep 1
 done 
-sudo apt-get -y update && apt-get install -y
+sudo apt-get update -y
 sudo apt-get install nginx -y
 systemctl enable --now nginx.service
